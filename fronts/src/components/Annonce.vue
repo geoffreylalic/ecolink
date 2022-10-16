eb4148053d64210d1a289512cc3b87108c75c2a3
Geoffrey <geoffrey0151@gmail.com>
2022-10-16 22:23:56 +0200

    connexion au back annonce

--- fronts/src/components/Annonce.vue
+++ fronts/src/components/Annonce.vue
@@ -1,27 +1,29 @@
 <template>
 
- <div class="md-layout md-gutter" style="margin-top:5%">
-    
+  <div class="md-layout md-gutter" style="margin-top:5%">
+
     <div class="md-layout-item md-size-50" style="margin-left:10% ">
-   <img src="./pexels1.jpg" style="border-radius:30px;border: solid;height:90%">
+      <img :src="image" style="border-radius:30px;border: solid;height:90%">
+      
     </div>
-    
+
     <div class="md-layout-item md-size-25" style="margin-right:15% ">
 
-        <div style="margin-bottom:10%;border-radius:10px;box-shadow: 2px 2px 2px 2px grey ;" >
-            <h2 style="color:black;">Contactez le vendeur</h2>
-            <h2 style="color:black;">Adecco</h2>
-            <md-button class="md-raised md-primary" style="width:80%;margin-top:6%">{{myObject.email}}</md-button> <br>
-            <md-button class="md-raised md-primary" style="width:80%">{{myObject.phone_number}}</md-button> <br>
-           
-        </div>
+      <div style="margin-bottom:10%;border-radius:10px;box-shadow: 2px 2px 2px 2px grey ;">
+        <h2 style="color:black;">Contactez le vendeur</h2>
+        <!-- <h2 style="color:black;">Admin</h2> -->
+        <md-button class="md-raised md-primary" style="width:80%;margin-top:6%">{{email}}</md-button> <br>
+        <md-button class="md-raised md-primary" style="width:80%">{{phone_number}}</md-button> <br>
+
+      </div>
 
     </div>
 
     <div class="md-layout-item md-size-85">
-            <h1 style="text-align: left;margin-left: 12.5%;color:black;" >{{myObject.name}}</h1> 
-            <p style="text-align: left;margin-left: 12.5%;color:black;">{{myObject.location}}</p> 
-            <h1 style="text-align: left;margin-left: 12.5%;color:black;">{{myObject.price}} €</h1> <br>
+      <h1 style="text-align: left;margin-left: 12.5%;color:black;">{{name}}</h1>
+      <p style="text-align: left;margin-left: 12.5%;color:black;">{{location}}</p>
+      <h1 style="text-align: left;margin-left: 12.5%;color:black;">{{price}} €</h1> 
+      <h1 style="text-align: left;margin-left: 12.5%;color:black;">{{quantity}} restant(s)</h1><br>
     </div>
 
     <div id="trait_dessus" style="
@@ -29,7 +31,7 @@
     border-top: 1px solid grey;
     width : 700px "> </div>
 
-     <div id="trait_dessus" style="
+    <div id="trait_dessus" style="
     margin-left: 11.5%;
     border-top: 1px solid grey;
     width : 700px;margin-top:5% "> </div>
@@ -38,13 +40,13 @@
 
 
     <div class="md-layout-item md-size-85" style="color:black">
-    <h1 style="text-align: left;margin-left: 12.5%;color:black;" >Critères : </h1> 
-           <div class="md-layout-item md-size-35">
-            <p style="margin-left: 2.5%;color:black;">Matiere  : {{myObject.category}}</p> 
-          </div>
-    <div class="md-layout-item md-size-25">
-              <p style="margin-left: 29.5%;color:black;">Type : {{myObject.type}} </p> 
-            </div>
+      <h1 style="text-align: left;margin-left: 12.5%;color:black;">Critères : </h1>
+      <div class="md-layout-item md-size-35">
+        <p style="margin-left: 2.5%;color:black;">Matiere : {{category}}</p>
+      </div>
+      <div class="md-layout-item md-size-25">
+        <p style="margin-left: 29.5%;color:black;">Type : {{type==='supply'? 'Offre': 'Demande'}} </p>
+      </div>
     </div>
 
 
@@ -54,15 +56,15 @@
     width : 700px "> </div>
 
     <div class="md-layout-item md-size-100" style="color:black">
-    <h1 style="text-align: left;margin-left: 10.5%;color:black;">Description</h1>
-    <p style="text-align: left;margin-left: 10.5%;color:black; margin-right:33%">{{description}} 
-</p>
+      <h1 style="text-align: left;margin-left: 10.5%;color:black;">Description</h1>
+      <p style="text-align: left;margin-left: 10.5%;color:black; margin-right:33%">{{description}}
+      </p>
     </div>
 
 
 
 
-     <div id="trait_dessus" style="
+    <div id="trait_dessus" style="
     margin-left: 11.5%;
     border-top: 1px solid grey;
     width : 700px;margin-top:5% "> </div>
@@ -72,12 +74,12 @@
 
 
 
-    
+
 </template>
 
      
 <script>
-//import publicationServices from '@/services/publicationServices';
+import publicationServices from '@/services/publicationServices';
 
 export default {
   name: 'AnnonceComponent',
@@ -87,28 +89,36 @@ export default {
 
   data: function () {
     return {
-      teste: 'hello teste',
-      publications: [],
-      
-      myObject: {
-      name: 'Nom annonce : ',
-      description: 'Jane Doe',
-      category: 'Matiere',
-      location: 'la ville',
-      type: 'type',
-      photos: '../pexels1.jpg',
-      price: '2016',
-      quantity: '2016-04-10',
-      email: 'myObject.email',
-      phone_number: '09 90 00 23 11'
-    }
-        };
+      name: null,
+      description: null,
+      category: null,
+      location: null,
+      type: null,
+      photos: null,
+      price: null,
+      quantity: null,
+      email: null,
+      phone_number: null,
+      image: null
+    };
   },
 
   mounted() {
     console.log("params id", this.$route.params.id)
-  //  publicationServices.getDetailPublication(this.$route.params.id).then((r)=> console.log(r))
-    
+    publicationServices.getDetailPublication(this.$route.params.id).then((r) => {
+      this.name = r.name
+      this.description = r.description
+      this.category = r.category
+      this.location = r.location
+      this.type = r.type
+      this.photos = r.photos
+      this.price = r.price
+      this.quantity = r.quantity
+      this.email = r.email
+      this.phone_number = r.phone_number
+      this.image = r.photos
+    })
+
   },
 }
 </script>
